22190f8eed95cf83f0c14e61ffbad244fcbfb42d
swetachovatiya23 <88458290+swetachovatiya23@users.noreply.github.com>
2022-03-11 17:32:55 -0500

    Update Home.js

--- finesse/src/pages/Home.js
+++ finesse/src/pages/Home.js
@@ -1,28 +1,24 @@
-
-
 import React from 'react';
 import Navbar from '../components/Navbar';
 import { BrowserRouter } from 'react-router-dom'
 import Announcement from '../components/Announcement';
-import Slider from '../components/Slider';
-import Categories from '../components/Categories';
-import Products from '../components/Products';
+import Footer from '../components/Footer'
+
+
 
 
 const Home = () => {
     return (
-    
-        <BrowserRouter>
+       <BrowserRouter>
             <Announcement/>
-            <Navbar/>
-            <Slider/>
-            <Categories/>
-            <Products/>
+             <Navbar/>
             
-
+             <Footer/>
         </BrowserRouter>
-     
+      
     )
 }
 
 export default Home;
+
+

af6fde6220bb6e5bc2a546723af412c7c46cc7c2
Venkatesh Bhamidipati <vbhamidipati@yahoo.com>
2022-03-12 16:16:28 -0500

    Fixed

--- finesse/src/components/Register.js
+++ finesse/src/components/Register.js
@@ -4,7 +4,7 @@ import { Search, ShoppingCartOutlined } from '@material-ui/icons';
 import React from 'react'
 import styled from 'styled-components'
 import SignIn from './SignIn.js'
-import Results from './Results'
+
 
 const Container2 = styled.div `
  height:0.01px;

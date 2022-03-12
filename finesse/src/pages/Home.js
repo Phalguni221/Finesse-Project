b1e22714b0fc904493f310947ceaa889c3354f0e
swetachovatiya23 <swetachovatiya2312@gmail.com>
2022-03-12 08:19:21 -0500

    Shopping cart added without functionality

--- finesse/src/pages/Home.js
+++ finesse/src/pages/Home.js
@@ -10,6 +10,7 @@ import Footer from '../components/Footer';
 import styled from 'styled-components';
 import Intro from '../components/Intro'
 import Newsletter from '../components/Newsletter';
+import Cart from '../components/Cart';
 
 
 
@@ -18,6 +19,7 @@ export default function Home () {
         <BrowserRouter>
             <Announcement/>
             <Navbar/>
+
             <br></br>
             <br></br>
             <br></br>
@@ -51,6 +53,7 @@ export default function Home () {
             <br></br>
             <br></br>
             <br></br>
+            <Cart/>
             <br></br>
             <br></br>
             <br></br>
@@ -65,6 +68,7 @@ export default function Home () {
         <p><Slider/></p>
             <br></br>
             <br></br>
+
             <Categories/>
             <Products/>
             <Newsletter/>

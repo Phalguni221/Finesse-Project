419e351b33833d6bd48f44ce5489b5176a76f9a0
swetachovatiya23 <swetachovatiya2312@gmail.com>
2022-03-14 22:02:21 -0400

    DataBase URL added

--- BE/index.js
+++ BE/index.js
@@ -1,9 +1,25 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+require('dotenv').config({ path: require('find-config')('.env') });
 
 
 
-app.listen(5000,() => {
-    console.log("Backend server is running");
 
-});
+mongoose
+  .connect(process.env.MONGO_URL) //for secret key
+  .then(() => console.log("DB Connection Successfull!"))  //.then for prom
+  .catch((err) => {
+    console.log(err); //For view errors
+  });
+
+
+  //Rest API Routes
+
+
+
+
+  app.listen(process.env.PORT || 3001, () => {
+    console.log("Backend server is running!");
+  });

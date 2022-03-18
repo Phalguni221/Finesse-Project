18cf066d61e25800bfc26eed1afd6734bc9bd357
Venkatesh Bhamidipati <vbhamidipati@yahoo.com>
2022-03-18 18:08:50 -0400

    Dress page came back

--- BE/Views/dresses/Edit.jsx
+++ BE/Views/dresses/Edit.jsx
@@ -1,33 +1,33 @@
-const React = require('react')
-const Def = require('../default')
+// const React = require('react')
+// const Def = require('../default')
 
 
-function Edit (data) {
-    return (
-        <Def>
-            <h1>Add a New Dress to the Collection!</h1>
-              {/* <form method="POST"  action={`/dresses/${data.dress.id}?_method=PUT`}>
-            <br></br> */}
-             <form>
-            <div className="form-group">
-                <label htmlFor="title">Dress Name</label>
-                 <input className="form-control" id="name" name="name" value = {data.Dress.title} required />
-            </div>
-            <br></br>
+// function Edit (data) {
+//     return (
+//         <Def>
+//             <h1>Add a New Dress to the Collection!</h1>
+//               {/* <form method="POST"  action={`/dresses/${data.dress.id}?_method=PUT`}>
+//             <br></br> */}
+//              <form>
+//             <div className="form-group">
+//                 <label htmlFor="title">Dress Name</label>
+//                  <input className="form-control" id="name" name="name" value = {data.Dress.title} required />
+//             </div>
+//             <br></br>
        
-            <div className="col-sm-6 col-md-4 col-lg-3">
-                <label htmlFor="pic">Dress Picture</label>
-                 <input className="dress-form" id="img" name="img" value = {data.Dress.img} required />
-                </div>
-            <br></br>
+//             <div className="col-sm-6 col-md-4 col-lg-3">
+//                 <label htmlFor="pic">Dress Picture</label>
+//                  <input className="dress-form" id="img" name="img" value = {data.Dress.img} required />
+//                 </div>
+//             <br></br>
 
-            <div className="col-sm-6 col-md-4 col-lg-3">
-                <label htmlFor="star">Star Rating</label>
-                 <input className="dress-form" id="star" name="star" value = {data.Dress.star} required />
-                </div>
-            <br></br>
-        </form>
-        </Def>
-            ) 
-        }
-        module.exports = Edit
+//             <div className="col-sm-6 col-md-4 col-lg-3">
+//                 <label htmlFor="star">Star Rating</label>
+//                  <input className="dress-form" id="star" name="star" value = {data.Dress.star} required />
+//                 </div>
+//             <br></br>
+//         </form>
+//         </Def>
+//             ) 
+//         }
+//         module.exports = Edit
--- BE/Views/dresses/index.jsx
+++ BE/Views/dresses/index.jsx
@@ -33,15 +33,17 @@ function index (data) {
          <form method="GET" action="cart">
              <text><input type="Submit" name="Add to Cart" id="Add to Cart" value="Add to Cart"/></text>
              </form> 
-             <form method="GET" action="cart">
-             <text><input type="Submit" name="Add to Cart" id="Add to Cart" value="Add to Cart"/></text>
-             </form> 
+          <br></br>
+          <br></br>
+              <p>
              <form method="GET" action="edit">
              <text><input type="Submit" name="Edit" id="Edit" value="Edit"/></text>
              </form> 
-          
+             </p>
+        <br></br>
+        <br></br>
+        <hr></hr>
          </div>
-
       </div> 
       </Def>
     )
@@ -51,9 +53,28 @@ function index (data) {
         <main>
             <h1>Finesse the Dress!</h1>
           {DressesFormatted}
+          <span class = "brlarge"></span>
+          <span class = "brlarge"></span>
+          <span class = "brlarge"></span>
+          <span class = "brlarge"></span>
+          <h1> Addressing Customer's AddDress</h1>
+    <p>Got any personal suggestions or favorites? Please add any choice of your dress that you like from our provider Unsplash! 
+      Click the button "AddDress" below to access 
+      Link to the dress section of Unsplash is located below. Copy the image link of any dress that you like!
+      Click the button "AddDress" below to access a submit form for the dress. 
+      Paste the link in the appropriate submission area and then provide your dress with a title and your personal star rating.
+       and paste it in the submit form below. Watch your preferred dress appear on screen below the dresses we have in Stokc
+      We promise to have it in stock within 5 business days for you to purchase!</p>
+      <form method="GET" action="AddDress">
+             <text><input type="Submit" name="AddDress" id="AddDress" value="AddDress"/></text>
+          
+             </form>
+             <br></br>
+             <br></br>
         </main>
   
 )
 }
+
 
 module.exports = index
